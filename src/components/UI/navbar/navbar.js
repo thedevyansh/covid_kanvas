c7f965c5d18bc52acf98093fa9a4bd94da0602f0
Akshat Kumar <55556128+Axhat@users.noreply.github.com>
2020-05-08 14:17:07 +0530

    navbar updation

--- src/components/UI/navbar/navbar.js
+++ src/components/UI/navbar/navbar.js
@@ -5,6 +5,7 @@ import corona from "../../../coronavirus.ico";
 import "./navbar.css";
 import "font-awesome/css/font-awesome.min.css";
 
+
 class NavBar extends React.Component {
   myFunction = () => {
     var x = document.getElementById("myTopnav");
@@ -32,15 +33,17 @@ class NavBar extends React.Component {
             COVID-19 INDIA
           </NavLink>
           <NavLink to="/faq/" exact className="Faq">
-            <i className="fa fa-question-circle" aria-hidden="true"></i> FAQs
+            <i className="fa" className="fa fa-question-circle" aria-hidden="true"></i> FAQs
           </NavLink>
           <NavLink to="/suggestion/" exact className="suggestions">
-            <i className="fa fa-lightbulb-o" aria-hidden="true"></i> Suggestions
+            <i className="fa" className="fa fa-lightbulb-o" aria-hidden="true"></i> Suggestions
           </NavLink>
           <NavLink to="/about" exact className="about">
-            <i className="fas fa-virus"></i>About COVID-19
+          <i className="fa" class="fa fa-hospital-o" aria-hidden="true"></i>
+About COVID-19
           </NavLink>
-          <a href="https://github.com/gawdsnitkkr/covid_kanvas">Contribute</a>
+          <a href="https://github.com/gawdsnitkkr/covid_kanvas"><i className="fa" class="fa fa-handshake-o" aria-hidden="true"></i>
+    Contribute</a>
           <button
             className="icon"
             onClick={this.myFunction}
